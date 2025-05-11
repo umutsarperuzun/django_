1d8f771b13018b6bbd1ae95d83ea0e4a7d0dee5b
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-11 13:50:39 +0100

    Set up Django app with dynamic course views and URL routing

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,4 +3,6 @@ from . import views # Import the views module from the current package
 
 urlpatterns = [
     path("",views.index,name="index" ),
+    path("<str:item>/",views.course,name="course")
+
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+course_dictionary = {
+    "python":"Python Course Page",
+    "java" : "Java Course Page",
+    "kotlin" : "Kotlin Course Page",
+    "swift" : "Swift Course Page"
+}
 def index(request):
     return HttpResponse("This is first Django project,first index")
+
+
+def course(request,item):
+    return HttpResponse(course_dictionary.get(item,"Not found"))
